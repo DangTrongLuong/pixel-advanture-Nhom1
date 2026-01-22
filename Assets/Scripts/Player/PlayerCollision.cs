69b3eadd35371ba94fbc3cd39ce0a6187a210732
KienVu1912 <vuhakien1912@gmail.com>
2026-01-22 17:59:44 +0700

    them sound items

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     private GameManager gameManager;
+    private AudioManager audioManager;
     private void Awake(){
         gameManager=FindAnyObjectByType<GameManager>();
+        audioManager=FindAnyObjectByType<AudioManager>();
     }
     private void OnTriggerEnter2D(Collider2D collision){
         if(collision.CompareTag("Items")){
             Destroy(collision.gameObject);
+            audioManager.PlayCoinSound();
             gameManager.AddScore(1);
         }
 
