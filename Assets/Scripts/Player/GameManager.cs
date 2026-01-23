1065524e85f91cae7c505ec634788af4cb17f29b
DangTrongLuong <dangtrongluong4@gmail.com>
2026-01-23 16:36:57 +0700

    add Player

---
+++ Assets/Scripts/Player/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject RandomPlayer()
+    {
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+
+        if (allPlayers.Length == 0)
+        {
+            Debug.LogWarning("Không tìm thấy player nào với tag 'Player'");
+            return null;
+        }
+
+        int randomIndex = Random.Range(0, allPlayers.Length);
+        GameObject selectedPlayer = allPlayers[randomIndex];
+
+        for (int i = 0; i < allPlayers.Length; i++)
+        {
+            if (i == randomIndex)
+                allPlayers[i].SetActive(true);
+            else
+                allPlayers[i].SetActive(false);
+        }
+
+        return selectedPlayer;
+    }
+}
